b37182035dd60d7c27c5c0c6346a52d5371b9f49
analy <anali.alejo@ucsp.edu.pe>
2023-06-23 16:09:49 -0500

    app with xamarin

---
+++ xamarin/App4/App4/view/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4.view
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_Clicked(object sender, EventArgs e)
+        {
+            string nombre = EntryNombre.Text;
+            string apellido = EntryApellido.Text;
+            string email= EntryEmail.Text;
+            string telf= EntryTelf.Text;
+            
+            await Application.Current.MainPage.Navigation.PushAsync(new Page2(nombre, apellido, email, telf));
+        }
+    }
+}
---
+++ xamarin/App4/App4/view/Page2.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4.view
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string nombre, string apellido, string email, string telf)
+        {
+            InitializeComponent();
+            labelNombre.Text = nombre + " " + apellido;
+            labelEmail.Text = email;
+            labelTelf.Text = telf;
+            
+        }
+
+        private async void Button2_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
